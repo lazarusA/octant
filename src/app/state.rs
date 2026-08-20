ab9f6f5f08a46b550819c0de3135a682d01cbb88
Lazaro Alonso <lazarus.alon@gmail.com>
2026-08-20 10:11:07 +0200

    local syntax

--- src/app/state.rs
+++ src/app/state.rs
@@ -415,10 +415,33 @@ impl OctantApp {
         //     crate::utils::diagnostics::log_gpu_diagnostics(wgpu_state);
         // }
 
-        Self {
+        let mut app = Self {
             wgpu_render_state: cc.wgpu_render_state.clone(),
             ..Default::default()
+        };
+
+        #[cfg(not(target_arch = "wasm32"))]
+        if let Some(target) = std::env::args().nth(1) {
+            let kind = if target.starts_with("http://")
+                || target.starts_with("https://")
+                || target.starts_with("s3://")
+            {
+                if target.to_lowercase().contains("icechunk") {
+                    StoreKind::RemoteIcechunk
+                } else {
+                    StoreKind::RemoteZarr
+                }
+            } else if target.to_lowercase().contains("icechunk") {
+                StoreKind::LocalIcechunk
+            } else {
+                StoreKind::LocalZarr
+            };
+            app.selected_store_kind = kind;
+            app.store_target_input = target;
+            app.inspect_active_store();
         }
+
+        app
     }
 
     pub fn reset_heatmap_view(&mut self) {
